d73a67286967ef5c1450875de7c87e4eb752d3ad
effractor <effractor@ce12c22d-465e-9864-fc69-74bad64dc156>
2010-12-16 08:56:11 +0000

    Add lost header :)

--- trunk/include/pdu.h
+++ trunk/include/pdu.h
@@ -12,91 +12,81 @@ Either version 2 of the License, or (at your option) any later version.
 #ifndef PDU_H
 #define PDU_H
 
-#include <string>
-
-using std::string;
-
 
 class PDU {
 public:
     enum NumerFormat {
         NF_UNKNOWN = 129,
         NF_INTERNATIONAL = 145,
-        NF_NATIONAL = 161
+        NF_INATIONAL = 161
     };
     
-    /*enum Alphabet {
+    enum Alphabet {
         GSM = -1,
         ISO = 0,
         BINARY = 1,
         UCS2 = 2
-    };*/
-    
-    enum Mode {
-        OLD,
-        NEW
     };
     
     // Constructors/Destructors
-    PDU() { m_flash = false; m_report = false; m_with_udh = false; }
-    PDU(const string& pdu);
-    virtual ~PDU() {}
+    PDU();
+    PDU(const char *pdu);
+    virtual ~PDU();
     
     // Getters
-    inline string getPDU() { return m_pdu; }
-    inline string getSender() { return m_sender; }
-    inline string getText() { return m_text; }
-    inline string getSMSC() { return m_smsc; }
-    inline string getLastError() { return m_err_msg; }
-    inline string getSenderAddressType() { return m_sender_addr_type; }
-    inline string getDate() { return m_date; }
-    inline string getTime() { return m_time; }
-    inline string getUDHType() { return m_udh_type; }
-    inline string getUDHData() { return m_udh_data; }
-    inline int getAlphabet() { return m_alphabet; }
-    inline string getAddress() { return m_address; }
+    inline const char* getPDU() const { return m_pdu; }
+    inline const char* getSMSC() const { return m_smsc; }
+    inline const char* getNumber() const { return m_number; }
+    inline const char* getNumberType() const { return m_number_type; }
+    inline const char* getDate() const { return m_date; }
+    inline const char* getTime() const { return m_time; }
+    inline const char* getUDHType() const { return m_udh_type; }
+    inline const char* getUDHData() const { return m_udh_data; }
+    inline const char* getMessage() const { return m_message; }
+    inline const char* getError() const { return m_err; }
     
     // Setters
-    inline void setPDU(const string pdu) { m_pdu = pdu; }
-    inline void setAddress(const string addr) { m_address = addr; }
-    inline void setText(const string text) { m_text = text; }
-    inline void setSMSC(const string smsc) { m_smsc = smsc; }
-    inline void setAlphabet(const int alpha) { m_alphabet = alpha; }
-    
-    // Staff
-    bool readSMSC();
-    bool splitDeliver();
-    bool splitStatusReport();
-    bool split();
-    int explainAddressType(const char *octet_char, int octet_int);
-    
-    void makePDU(string mode, int validity, int replace_msg, int system_msg);
-    
+    void setMessage(const char* message, const int message_len = -1);
+    void setSMSC(const char* smsc);
+    void setNumber(const char* number);
+    void setAlphabet(const Alphabet alphabet);
 
+    // 
+    bool parse();
+    void generate();
+    
+    // iconv
+    int convert(const char *tocode, const char *fromcode);
 private:
-    NumerFormat m_number_frmt;
-    string m_pdu;
-    const char *m_pdu_ptr; // Pointer to chars into m_pdu. For internal use only
-    string m_err_msg;
-    string m_sender;
-    string m_sender_addr_type;
-    string m_text;
-    string m_date;
-    string m_time;
-    int m_alphabet;
-    string m_smsc; 
-    Mode m_mode;
-    int m_replace;  //TODO: remove it?
-    bool m_flash;
-    bool m_report;
-    
+    char* m_pdu;
+    char* m_pdu_ptr;
+    char* m_message;
+    int m_message_len;
+    char* m_smsc; 
+    char* m_number;
+    char* m_number_type;
+    NumerFormat m_number_fmt;
+    char* m_date;
+    char* m_time;
+    char* m_udh_type;
+    char* m_udh_data;
+    char* m_err;
     bool m_with_udh;
-    string m_udh_type;
-    string m_udh_data;
+    bool m_report;
+    bool m_is_statusreport;
+    int m_replace;
+    int m_alphabet;
+    bool m_flash;
+    const char* m_mode;
+    int m_validity;
+    int m_system_msg;
+    int m_replace_msg;
     
-    bool m_is_statusreport;  
-    
-    string m_address;  
+    void reset();
+    bool parseSMSC();
+    bool parseDeliver();
+    bool parseStatusReport();
+    int explainAddressType(const char *octet_char, int octet_int);
 };
 
 #endif
